6dc327993dd62b5da322622861694f47e5b0fe7b
kush <kushagrathakur11@gmail.com>
2017-03-06 23:45:33 +0000

    Initial model with helper funcs

---
+++ helper.py
@@ -0,0 +1,74 @@
+import errno
+import json
+import os
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.misc
+from scipy.stats import bernoulli
+
+
+def crop(image, top_percent, bottom_percent):
+    top = int(np.ceil(image.shape[0] * top_percent))
+    bottom = image.shape[0] - int(np.ceil(image.shape[0] * bottom_percent))
+    return image[top:bottom, :]
+
+def resize(image, new_dim):
+    return cv2.resize(image, new_dim) 
+
+def random_flip(image, steering_angle, flipping_prob=0.5):
+    head = bernoulli.rvs(flipping_prob)
+    if head:
+        return cv2.flip(image,1), -1 * steering_angle
+    else:
+        return image, steering_angle
+
+def generate_new_image(image, steering_angle, top_crop_percent=0.35, bottom_crop_percent=0.1,
+                       resize_dim=(64, 64)):
+    image = crop(image, top_crop_percent, bottom_crop_percent)
+    image, steering_angle = random_flip(image, steering_angle)
+    image = resize(image, resize_dim)
+    return image, steering_angle
+
+def get_next_image_files(batch_size=64):
+    data = pd.read_csv('./data/driving_log.csv')
+    num_of_img = len(data)
+    rnd_indices = np.random.randint(0, num_of_img, batch_size)
+
+    image_files_and_angles = []
+    for index in rnd_indices:
+        rnd_image = np.random.randint(0, 3)
+        if rnd_image == 0:
+            img = data.iloc[index]['left'].strip()
+            angle = data.iloc[index]['steering'] + 0.25
+            image_files_and_angles.append((img, angle))
+        elif rnd_image == 1:
+            img = data.iloc[index]['center'].strip()
+            angle = data.iloc[index]['steering']
+            image_files_and_angles.append((img, angle))
+        else:
+            img = data.iloc[index]['right'].strip()
+            angle = data.iloc[index]['steering'] - 0.25
+            image_files_and_angles.append((img, angle))
+    return image_files_and_angles
+
+def generate_next_batch(batch_size=64):
+    while True:
+        X_batch = []
+        y_batch = []
+        images = get_next_image_files(batch_size)
+        for img_file, angle in images:
+            raw_image = plt.imread('./data/'+ img_file)
+            raw_angle = angle
+            new_image, new_angle = generate_new_image(raw_image, raw_angle)
+            X_batch.append(new_image)
+            y_batch.append(new_angle)
+        yield np.array(X_batch), np.array(y_batch)
+
+def save_model(model, model_name='model.json', weights_name='model.h5'):
+    json_string = model.to_json()
+    with open(model_name, 'w') as outfile:
+        json.dump(json_string, outfile)
+
+    model.save_weights(weights_name)
---
+++ model.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+from keras.layers import Dense, Flatten, Lambda, Activation, MaxPooling2D
+from keras.layers.convolutional import Convolution2D
+from keras.models import Sequential
+from keras.optimizers import Adam
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+import helper
+#from keras.utils.visualize_util import plot
+
+tf.python.control_flow_ops = tf
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(64, 64, 3)))
+model.add(Convolution2D(24, 5, 5, border_mode='same', subsample=(2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+model.add(Convolution2D(36, 5, 5, border_mode='same', subsample=(2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+model.add(Convolution2D(48, 5, 5, border_mode='same', subsample=(2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+model.add(Flatten())
+
+model.add(Dense(1164))
+model.add(Activation('relu'))
+model.add(Dense(100))
+model.add(Activation('relu'))
+model.add(Dense(50))
+model.add(Activation('relu'))
+model.add(Dense(10))
+model.add(Activation('relu'))
+model.add(Dense(1))
+model.summary()
+model.compile(loss="mse",optimizer=Adam(1e-4), metrics=['accuracy'])
+#plot(model, to_file='model.png')
+
+train_gen = helper.generate_next_batch()
+validation_gen = helper.generate_next_batch()
+history = model.fit_generator(train_gen,samples_per_epoch=20032, nb_epoch=8,validation_data=validation_gen,nb_val_samples=6400,verbose=1)
+
+
+# list all data in history
+print(history.history.keys())
+
+# summarize history for accuracy
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(history.history['acc'])
+ax.plot(history.history['val_acc'])
+ax.set_title('model accuracy')
+ax.set_ylabel('accuracy')
+ax.set_xlabel('epoch')
+ax.legend(['train', 'test'], loc='upper left')
+fig.savefig('modelAccuracy.png')
+
+helper.save_model(model)
